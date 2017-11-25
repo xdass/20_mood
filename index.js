f27785556089f86efb2647655500f2af9043c8f1
xdass <420600@gmail.com>
2017-11-25 12:53:41 +0500

    Add background image and Google Font Lobster

--- index.js
+++ index.js
@@ -6,8 +6,8 @@ function get_random_number(quotes_length) {
 }
 
 let json_quotes = JSON.parse(document.getElementById('quotes_json').textContent);
-let phrase_div = document.querySelector('.phrase');
-let signature_div = document.querySelector('.signature')
+let phrase_div = document.querySelector('.js-phrase');
+let signature_div = document.querySelector('.js-signature')
 
 
 const number = get_random_number(json_quotes.length);
